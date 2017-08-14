47da50dfd2110276742445881d20cffdcf20e9fb
Thiago de Bastos <thiago@thiagodebastos.com>
2017-08-14 16:13:31 +1000

    fix issues with some graph animations

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@ import ScrollMagic from "scrollmagic";
 const TL = new TimelineMax();
 
 TL.set(".bar-graph-3 rect, .bar-graph-2--bar", {
-  autoAlpha: 0
+  autoAlpha: 0,
 });
 
 //
@@ -29,12 +29,11 @@ function Scroller(scrollElement, scrollTarget) {
   });
 }
 
-function animateGraph(graphID) {
+function animateBarChart(graphID) {
   const TL = new TimelineLite();
-
   const rects = nodeListToArray(`${graphID} rect.fill`);
   rects.map(rect => {
-    return TL.to(rect, 0.1, {
+    return TL.to(rect, 0.2, {
       fill: "#005689"
     });
   });
@@ -80,24 +79,68 @@ function animateNumberArray(target, stats) {
   });
 }
 
-// Tooltip highlighting
-// let highlighted = false;
-// const chart1Primary = document.querySelector(".donut-chart-2 .st2");
-// const chart1Tooltips = document.querySelector(".tile--left .tooltip");
-
-// function highlight(target) {
-//   console.log(target);
-//   target.style.color = highlighted ? "red" : "rebeccapurple";
-// }
-
-// chart1Primary.addEventListener("mouseover", () => highlight(chart1Tooltips));
-// chart1Primary.addEventListener("mouseout", () => (highlighted = false));
-
 //
 // ─── SCROLLMAGIC ────────────────────────────────────────────────────────────────
 //
 
 function scrollMagicCustomGraph_01() {
+  const controller = new ScrollMagic.Controller();
+  const scene = new ScrollMagic.Scene({
+      triggerElement: ".bar-graph-1",
+      duration: 250,
+      reverse: false
+    })
+    .on("start", () =>
+      TL.set(".bar-graph-1 rect, .bar-graph-1 line", {
+        autoAlpha: 1
+      })
+      .staggerTo(
+        ".bar-graph-1 rect",
+        0.5, {
+          autoAlpha: 1,
+          ease: Power1.easeIn
+        },
+        0.1
+      )
+      .to(".bar-graph-1 text", 0.25, {
+        autoAlpha: 1
+      })
+      .from(".bar-graph-1 line", 0.25, {
+        scale: 0,
+        transformOrigin: "center center"
+      })
+    )
+    .addTo(controller);
+}
+
+
+function scrollMagicCustomGraph_02() {
+  const controller = new ScrollMagic.Controller();
+  const scene = new ScrollMagic.Scene({
+      triggerElement: ".bar-graph-2",
+      duration: 250,
+      reverse: false
+    })
+    .on("start", () =>
+      TL.set(".bar-graph-2--bar", {
+        autoAlpha: 1
+      })
+      .staggerFrom(
+        ".bar-graph-2--bar",
+        0.5, {
+          scaleX: 0,
+          transformOrigin: "left center",
+          ease: Power1.easeIn
+        },
+        0.1
+      )
+      .to(".bar-graph-2 text", 0.25, {
+        autoAlpha: 1
+      }))
+    .addTo(controller);
+}
+
+function scrollMagicCustomGraph_03() {
   const controller = new ScrollMagic.Controller();
   const scene = new ScrollMagic.Scene({
       triggerElement: ".bar-graph-3",
@@ -128,36 +171,10 @@ function scrollMagicCustomGraph_01() {
     .addTo(controller);
 }
 
-function scrollMagicCustomGraph_02() {
+function scrollMagicGraphs(externalAnimation, target, triggerElement) {
   const controller = new ScrollMagic.Controller();
   const scene = new ScrollMagic.Scene({
-      triggerElement: ".bar-graph-2",
-      duration: 250,
-      reverse: false
-    })
-    .on("start", () =>
-      TL.set(".bar-graph-2--bar", {
-        autoAlpha: 1
-      })
-      .staggerFrom(
-        ".bar-graph-2--bar",
-        0.5, {
-          scaleX: 0,
-          transformOrigin: "left center",
-          ease: Power1.easeIn
-        },
-        0.1
-      )
-      .to(".bar-graph-2 text", 0.25, {
-        autoAlpha: 1
-      }))
-    .addTo(controller);
-}
-
-function scrollMagicGraphs(externalAnimation, target) {
-  const controller = new ScrollMagic.Controller();
-  const scene = new ScrollMagic.Scene({
-      triggerElement: "#stats_01",
+      triggerElement,
       duration: 300,
       reverse: false
     })
@@ -184,16 +201,10 @@ function scrollMagicAnimations(externalAnimation) {
 
   function init() {
     targets = [{
-        triggerNode: "#stats_02",
-        animationTarget: nodeListToArray("#stats_02 .stat-number"),
-        stats: [6]
-      },
-      {
-        triggerNode: "#stats_03",
-        animationTarget: nodeListToArray("#stats_03 .stat-number"),
-        stats: [55, 66, 58, 61]
-      }
-    ];
+      triggerNode: "#stats_02",
+      animationTarget: nodeListToArray("#stats_02 .stat-number"),
+      stats: [6]
+    }];
 
     animateEverything(targets, animateNumberArray);
   }
@@ -211,10 +222,10 @@ function App() {
   scrollDownButton = document.querySelector(".scroll-down");
   Scroller(scrollDownButton, "#content");
   scrollMagicAnimations(animateNumberArray).init;
-  scrollMagicGraphs(animateGraph, "#bar-graph-1");
-  scrollMagicGraphs(animateGraph, "#bar-graph-2");
+  scrollMagicGraphs(animateBarChart, '#bar-graph-1', '#stats_02');
   scrollMagicCustomGraph_01();
   scrollMagicCustomGraph_02();
+  scrollMagicCustomGraph_03();
 }
 
 let scrollDownButton;
